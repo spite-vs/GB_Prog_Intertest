f0e0adca95b46b454c3110a05f18c4008ca50b29
Spite <spite-vs@yandex.ru>
2023-10-07 15:30:10 +0300

    homework 1 - fix

--- hw_1/notes.py
+++ hw_1/notes.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 
 def err():
-    print('\n -- Открой глаза шире и нажимай на клавиши с умом --\n')
+    print('\n -- Открой глаза шире и нажимай на клавиши с умом --')
 
 
 def end():
-    print('\n -- Возвращайся, я буду ждать --\n')
+    print('\n -- Возвращайся, я буду ждать --')
 
 
 def title():
@@ -38,7 +38,7 @@ def wrt(dt):
 
 
 def input_data():
-    buffer = f'{index()};{title()};{body_note()};{datetime.now()}\n'
+    buffer = f'{index()};{title()};{body_note()};{str(datetime.now()).split(".")[0]}\n'
     if not os.path.isfile('notes.csv'):
         with open('notes.csv', 'a', encoding='utf-8', newline='') as pb:
             pb.write('№;Title;Body_note;Date_time\n')
@@ -53,15 +53,27 @@ def print_data():
     with open('notes.csv', 'r', encoding='utf-8', newline='') as pb:
         reader = csv.DictReader(pb, delimiter=';')
         var = list(reader)
+        com = input('\nОтсортировать по дате? (Y/N)(Д/Н)')
+    while com not in 'yYnNдДнН':
+        err()
+        com = input(
+            '\nОтсортировать по дате? (Y/N)(Д/Н)')
+    if com in 'nNнН':
         print()
         for i in var:
             print(
                 f'{i["№"]:>3}. {i["Title"]} ({i["Date_time"]})')
         print()
-        num = int(input('Под каким номером надо что-то перечитать: '))
-    while num < 1 or num > len(var):
+    else:
+        for i in sorted(var, key=lambda x: datetime.strptime(x['Date_time'], '%Y-%m-%d %H:%M:%S')):
+            print(
+                f'{i["№"]:>3}. {i["Title"]} ({i["Date_time"]})')
+        print()
+    num = input('Под каким номером надо что-то перечитать: ')
+    while not num.isdigit() or int(num) < 1 or int(num) > len(var):
         err()
-        num = int(input('Под каким номером надо что-то перечитать: '))
+        num = input('Под каким номером надо что-то перечитать: ')
+    num = int(num)
     print(
         f'\n{var[num-1]["Title"]}: \n\t{var[num-1]["Body_note"]}')
 
@@ -75,17 +87,18 @@ def put_data():
             print(
                 f'{i["№"]:>3}. {i["Title"]} ({i["Date_time"]})')
         print()
-    number = int(input('Какую заметку вздумалось поменять: '))
-    while number < 1 or number > len(var):
+    number = input('\nКакую заметку вздумалось поменять: ')
+    while not number.isdigit() or int(number) < 1 or int(number) > len(var):
         err()
-        number = int(input('Какую заметку вздумалось поменять: '))
-    key_data = {'2': 'Title', '3': 'Body_note', '4': 'Date_time'}
-    key_operation = {'2': title, '3': body_note, '4': datetime.now}
+        number = input('\nКакую заметку вздумалось поменять: ')
+    number = int(number)
+    key_data = {'2': 'Title', '3': 'Body_note'}
+    key_operation = {'2': title, '3': body_note}
     print(
         f'\n{var[number-1]["Title"]}: \n\t{var[number-1]["Body_note"]}')
     print('\nЧто переписываем?\n\n1. Всё\n2. Заголовок\n3. Заметку\n4. Стой! Я передумал')
     com = input("Введите номер операции: ")
-    while com < '1' or com > '4':
+    while not com.isdigit() or int(com) < 1 or int(com) > 4:
         err()
         print(
             '\nЧто переписываем?\n\n1. Всё\n2. Заголовок\n3. Заметку\n4. Стой! Я передумал')
@@ -95,11 +108,12 @@ def put_data():
     elif com == '1':
         for i in key_data:
             var[number-1][key_data[i]] = key_operation[i]()
+            var[number-1]['Date_time'] = str(datetime.now()).split(".")[0]
         wrt(var)
         print('\n -- Переписал --')
     else:
         var[number-1][key_data[com]] = key_operation[com]()
-        var[number-1][key_data['4']] = key_operation['4']()
+        var[number-1]['Date_time'] = str(datetime.now()).split(".")[0]
         wrt(var)
         print('\n -- Переписал --')
 
@@ -113,17 +127,18 @@ def delete_data():
             print(
                 f'{i["№"]:>3}. {i["Title"]} ({i["Date_time"]})')
         print()
-    number = int(input('От чего решил избавиться: '))
-    while number < 1 or number > len(var):
+    number = input('От чего решил избавиться: ')
+    while not number.isdigit() or int(number) < 1 or int(number) > len(var):
         err()
-        number = int(input('От чего решил избавиться: '))
+        number = input('От чего решил избавиться: ')
+    number = int(number)
     print(
         f'\n{var[number-1]["Title"]}: \n\t{var[number-1]["Body_note"]}')
-    com = input('\nУверен, что это она? (Y/N)(Д/Н)')
+    com = input('\nУверен, что это оно? (Y/N)(Д/Н)')
     while com not in 'yYnNдДнН':
         err()
         com = input(
-            '\nУверен, что это она? (Y/N)(Д/Н)')
+            '\nУверен, что это оно? (Y/N)(Д/Н): ')
     if com in 'nNнН':
         end()
     else:
